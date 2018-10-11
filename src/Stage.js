b837cda4a1307dfdb0f732d969d01c67b0e4bca2
Raymond Cook <lunarraid@gmail.com>
2018-10-11 18:32:36 -0400

    Fix compatibility bug with reconciler

--- src/Stage.js
+++ src/Stage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ReactPixiLayout } from './index';
 import Application from './elements/Application';
+import { version } from '../package.json';
 
 export default class Stage extends React.Component {
 
@@ -35,16 +36,16 @@ export default class Stage extends React.Component {
     ReactPixiLayout.injectIntoDevTools({
       findFiberByHostInstance: ReactPixiLayout.findFiberByHostInstance,
       bundleType: 1,
-      version: '0.0.1',
+      version,
       rendererPackageName: 'react-pixi-layout'
     });
 
-    ReactPixiLayout.updateContainer(this.props.children, this._applicationContainer, this);
+    ReactPixiLayout.updateContainer(this.props.children, this._applicationContainer);
   }
 
   componentDidUpdate (prevProps, prevState) {
     this._applicationElement.applyProps(prevProps, this.props);
-    ReactPixiLayout.updateContainer(this.props.children, this._applicationContainer, this);
+    ReactPixiLayout.updateContainer(this.props.children, this._applicationContainer);
   }
 
   componentWillUnmount () {
